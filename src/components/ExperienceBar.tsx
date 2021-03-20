fdd339af8b40369a4343b062431bbcbcea488045
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-03-19 23:30:33 -0400

    estrategia de CSS para o next

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.ExperienceBar}>
       <span>8 xp</span>
         <div>
           <div style={{ width: '50%' }} />
-          <span className="current-experience" style={{ left: '50%' }}>
-            400 xp
+          <span className={styles.currentExperience} style={{ left: '50%' }}>
+            300 xp
           </span>
         </div>
       <span>600 xp</span>

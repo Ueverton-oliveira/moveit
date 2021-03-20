14d5ad347911e05afbcc6a043b70874903475c6f
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-03-19 23:21:47 -0400

    document_typeScript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 import { ExperienceBar } from '../components/ExperienceBar'
+
 export default function Home() {
   return (
     <div className="container">
+      
       <ExperienceBar />
     </div>
   )

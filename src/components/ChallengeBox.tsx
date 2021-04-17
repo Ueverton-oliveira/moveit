dfe03783ac0080f22a4cea6f39599d8df8bebe3a
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-04-16 20:35:06 -0400

    hasActiveChallenge

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,20 @@
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
+    const hasActiveChallenge = true
     return (
         <div className={styles.challengeBoxContainer}>
-            <div className={styles.challengeNotActive}>
-                <strong>Finalize um ciclo para receber um desafio</strong>
-                <p>
-                    <img src="" alt=""/>
-                    Avance de level completando desafios.
-                </p>
-            </div>
+            { hasActiveChallenge ?(
+                <div/>
+            ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        <img src="" alt=""/>
+                        Avance de level completando desafios.
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
